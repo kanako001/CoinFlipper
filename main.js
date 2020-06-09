28cb740eda3199aac2b6e22ebcc43c092e2fef49
kanako001 <kunleanako@gmail.com>
2020-06-09 09:26:09 -0400

    Restructured the project by creating a base Game class which the coinflipper game class inherits from. Also started the game flow process

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-class CoinFlipper {
+class Game {
 
-  constructor(wallet = 0, bettingPrice = 0, opponentNumber = 0) {
+  constructor(wallet = 0, roundNumber = 0, opponentNumber = 0) {
     this.wallet = wallet;
-    this.bettingPrice = bettingPrice;
+    this.roundNumber = roundNumber;
     this.opponentNumber = opponentNumber;
     this.coinWeight = { "head": 50, "tail": 50 };
   };
+
+  basicInfo() {
+    let name = prompt(`what's your name?`);
+    this.opponentNumber = prompt(`How many opponents would you like to play against?`);
+    return [name, this.opponentNumber]
+  }
 
   keysGenerator() {
     let keysContainer = [];
@@ -16,12 +22,12 @@ class CoinFlipper {
         keysContainer.push(key);
       }
     });
-    return keysContainer
+    return keysContainer;
   } 
 
 
-  randomizer() {
-    return this.keysGenerator()[Math.floor((Math.random()*this.keysGenerator().length))]
+  randomArrayItem(array) {
+     console.log(array[Math.floor((Math.random()*array.length))]);
   }
 
   addToWallet() {
@@ -29,10 +35,33 @@ class CoinFlipper {
     this.wallet = prompt(`How much would you like to your wallet?\n`);
     return this.wallet;
   }
-
+  
 
 }
 
 
-const myTest = new CoinFlipper()
-console.log(myTest.addToWallet());
+const myTest = new Game();
+// console.log(myTest.gameFlow());
+
+
+
+class CoinFlipper extends Game {
+
+  cpu() {
+    let options = ["heads", "heads", "tail", "heads", "tail" ];
+    let cpuChoice = this.randomArrayItem(options)
+    return cpuChoice
+  }
+
+  gameFlow() {
+      
+    console.log("EACH GAME COSTS $50. THE WINNER TAKES ALL. YOU WILL BE PLAYING ONE OPPONENT PER ROUND WITHIN EACH GAME. EACH PLAYER WILL MAKE A GUESS, IF YOU'RE CORRECT, YOU CAN DECIDE TO KEEP PLAYING OR CASH OUT. \n")
+    const [name, opponents] = this.basicInfo();
+    console.log(`\nWELCOME TO THE GAME OF CHANCE AND PROBABILITY. MAY THE ODDS BE WITH YOU ${name.toUpperCase()}. LET'S FFFLLLIIIPPP!!!`);
+
+  }
+
+}
+
+const sam = new CoinFlipper();
+sam.gameFlow();
