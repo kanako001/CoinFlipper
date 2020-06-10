2e1db6bcbabacf7c3375c2674b9d0949c96ae23b
kanako001 <kunleanako@gmail.com>
2020-06-10 08:52:06 -0400

    Completed the game flow portion of the coinFlipper

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ class Game {
     this.coinWeight = { "head": 50, "tail": 50 };
   };
 
+  cpu() {
+    let options = ["head", "head", "tail", "head", "tail" ];
+    let cpuChoice = this.randomArrayItem(options)
+    return cpuChoice
+  }
+
   basicInfo() {
     let name = prompt(`what's your name?`);
     this.opponentNumber = prompt(`How many opponents would you like to play against?`);
@@ -27,7 +33,7 @@ class Game {
 
 
   randomArrayItem(array) {
-     console.log(array[Math.floor((Math.random()*array.length))]);
+    return array[Math.floor((Math.random()*array.length))];
   }
 
   addToWallet() {
@@ -40,25 +46,39 @@ class Game {
 }
 
 
-const myTest = new Game();
-// console.log(myTest.gameFlow());
-
-
+// const myTest = new Game();
+// console.log(myTest.cpu());
 
 class CoinFlipper extends Game {
 
-  cpu() {
-    let options = ["heads", "heads", "tail", "heads", "tail" ];
-    let cpuChoice = this.randomArrayItem(options)
-    return cpuChoice
-  }
-
   gameFlow() {
       
-    console.log("EACH GAME COSTS $50. THE WINNER TAKES ALL. YOU WILL BE PLAYING ONE OPPONENT PER ROUND WITHIN EACH GAME. EACH PLAYER WILL MAKE A GUESS, IF YOU'RE CORRECT, YOU CAN DECIDE TO KEEP PLAYING OR CASH OUT. \n")
+    console.log("EACH GAME COSTS $50. THE WINNER TAKES ALL. YOU WILL BE PLAYING ONE OPPONENT PER ROUND WITHIN EACH GAME. EACH PLAYER WILL MAKE A GUESS, IF YOU'RE CORRECT, YOU CAN DECIDE TO KEEP PLAYING OR CASH OUT. TO WIN, YOUR GUESSES HAVE TO BE CORRECT MORE TIMES THAN YOUR OPPONENT. \n")
     const [name, opponents] = this.basicInfo();
     console.log(`\nWELCOME TO THE GAME OF CHANCE AND PROBABILITY. MAY THE ODDS BE WITH YOU ${name.toUpperCase()}. LET'S FFFLLLIIIPPP!!!`);
+    
+    let count = 0
+    let userWinnings = 0;
+    let cpuWinnings = 0;
 
+    while (count < Number(opponents)) {
+      let cpuGuess = this.cpu()
+      let userGuess = prompt(`head or tail: `).toLowerCase()
+      let answer = this.randomArrayItem(this.keysGenerator());
+      console.log(`cpu: ${cpuGuess} \nuser: ${userGuess} \nanswer: ${answer}\n`)
+      // if (userGuess !== 'head' || userGuess !== 'tail') return 'Not an option.';
+      if (userGuess === answer) { userWinnings += 1 }
+      if (cpuGuess === answer) { cpuWinnings +=1 }
+      count++
+    }
+
+    if (userWinnings > cpuWinnings)  console.log(`${name}!!! you won.`);
+    if (cpuWinnings > userWinnings)  console.log(`cpu won meep morp`);
+    if (cpuWinnings === userWinnings)  console.log(`'tis a tie`);
+
+    let playOption = prompt(`Would you like to play again? Y or N: `).toLowerCase();
+    if (playOption === 'y') return this.gameFlow();
+    if (playOption === 'n') return 'thanks for playing.'
   }
 
 }
